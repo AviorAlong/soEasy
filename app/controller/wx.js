ff2ca08a075670717016e457892c60a6ea019b05
shial <along.shi@zhangmen.com>
2019-06-26 20:18:36 +0800

    wx

---
+++ app/controller/wx.js
@@ -0,0 +1,37 @@
+const Controller = require('egg').Controller;
+
+class WXController extends Controller {
+    async index() {
+        var query = url.parse(req.url, true).query;
+        console.log("*** URL:" + req.url);
+        console.log(query);
+        var signature = query.signature;
+        var echostr = query.echostr;
+        var timestamp = query['timestamp'];
+        var nonce = query.nonce;
+        var oriArray = new Array();
+        oriArray[0] = nonce;
+        oriArray[1] = timestamp;
+        oriArray[2] = config.wechat_validate.token; //微信开发者中心页面里填的token
+        oriArray.sort();
+        var original = oriArray.join('');
+        console.log("Original str : " + original);
+        console.log("Signature : " + signature);
+        var scyptoString = sha1(original);
+        if (signature == scyptoString) {
+            res.end(echostr);
+            console.log("Confirm and send echo back");
+        } else {
+            res.end("false");
+            console.log("Failed!");
+        }
+    };
+    sha1(str) {
+        var md5sum = crypto.createHash("sha1");
+        md5sum.update(str);
+        str = md5sum.digest("hex");
+        return str;
+    }
+}
+
+module.exports = WXController;
