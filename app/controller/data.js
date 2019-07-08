7263a599b7d7084a2c5bf59edf445b634c3251c3
shial <along.shi@zhangmen.com>
2019-07-08 19:36:23 +0800

    tt

--- app/controller/data.js
+++ app/controller/data.js
@@ -10,7 +10,7 @@ class DataController extends Controller {
       //查库
       let r = await ctx.model.Rubbish.findByName(r_name);
       if(r){
-        await ctx.model.Rubbish.update({where:{id:r.id}},{r_content:body.r_content})
+        await ctx.model.Rubbish.update({r_content:body.r_content},{where:{id:r.id}})
       }else{
         await ctx.model.Rubbish.insert(body)
       }
